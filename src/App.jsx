a754e603f3df4dea42babd30133ab850eb33c1d9
SneatX <santi02005@hotmail.com>
2024-10-02 16:25:35 -0500

    fix: :ambulance: Error por importación de archivo no existente

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/LoginPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
