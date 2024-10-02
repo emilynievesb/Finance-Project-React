29f1d830f82b9c0c7abdd59f5c96fcdd46e54cf8
SneatX <santi02005@hotmail.com>
2024-10-02 18:57:23 -0500

    fix: :ambulance: Se realiza corrección de logica que se usaba en el hook

--- src/components/SelectMenu.jsx
+++ src/components/SelectMenu.jsx
@@ -5,14 +5,12 @@ import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@h
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
 export default function SelectMenu({ selected, setSelected, items }) {
-    // const [selected, setSelected] = useState(people[3]);
-
     return (
-        <Listbox value={selected} onChange={setSelected}>
+        <Listbox value={selected} onChange={(value) => setSelected(value.key)}>
             <div className="relative w-[100%] ">
                 <ListboxButton className="relative w-[100%] cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
                     <span className="flex items-center">
-                        <span className="ml-3 block truncate"> {selected} </span>
+                        <span className="ml-3 block truncate"> {items.find((item) => item.key === selected)?.label || '-'} </span>
                     </span>
                     <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
                         <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
@@ -23,14 +21,14 @@ export default function SelectMenu({ selected, setSelected, items }) {
                     transition
                     className="absolute z-10 mt-1 max-h-56 w-[100%] overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
                 >
-                    {items.map((item, id) => (
+                    {items.map((item) => (
                         <ListboxOption
-                            key={id + 1}
+                            key={item.id}
                             value={item}
                             className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
                         >
                             <div className="flex items-center">
-                                <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">{item}</span>
+                                <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">{item.label}</span>
                             </div>
 
                             <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
