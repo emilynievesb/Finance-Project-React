94b6f8308096035bc43d77c34f18827f9d8571ed
emilynievesb <nemily1710@gmail.com>
2024-10-04 12:26:45 -0500

    feat: :sparkles: Se realizan servicios fetch a la base de datos para obtener el total de las transacciones y tambien de manera paginada

--- src/shared/services/transactionService.js
+++ src/shared/services/transactionService.js
@@ -23,3 +23,46 @@ export const createTransaction = async (transactionData) => {
         throw new Error(error.message || 'Error de red o servidor');
     }
 };
+export const getTransactionsByUserId = async (userID) => {
+    try {
+        const response = await fetch(`${apiUrl}/transacciones/usuario/${userID}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || 'Error al obtener las transacciones');
+        }
+
+        return data; // Devolvemos los datos de las transacciones
+    } catch (error) {
+        throw new Error(error.message || 'Error de red o servidor');
+    }
+};
+
+export const getTransactionsWithPagination = async (userID, page = 1, limit = 10) => {
+    try {
+        const response = await fetch(`${apiUrl}/transacciones/pag/usuario/${userID}?page=${page}&limit=${limit}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || 'Error al obtener las transacciones');
+        }
+
+        return data; // Devolvemos los datos de las transacciones con paginación
+    } catch (error) {
+        throw new Error(error.message || 'Error de red o servidor');
+    }
+};
