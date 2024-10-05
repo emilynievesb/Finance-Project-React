d740e287651a42b103b15d21aed9ef566532aedb
emilynievesb <nemily1710@gmail.com>
2024-10-05 14:56:33 -0500

    feat: :sparkles: Se agrega nuevo componente a cargo de los graficos

--- src/components/ContentFinance.jsx
+++ src/components/ContentFinance.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import AddRecordForm from './AddRecordForm';
 import SummaryTable from './SummaryTable';
-// import { useHandleViewTransactionsUser } from '../shared/hooks/useHandleViewTransactionsUser';
-import { useHandleTransactionsPagination } from '../shared/hooks/useHandleTransactionsPagination';
+import Graphics from './Graphics';
 
 export default function ContentFinance({ activeSection, userID }) {
     return (
         <div className="flex-1">
-            {activeSection === 0 && <h1 className="text-3xl">Bienvenido al Dashboard</h1>}
+            {activeSection === 0 && <Graphics userID={userID} />}
             {activeSection === 1 && <AddRecordForm userID={userID} />}
             {activeSection === 2 && <SummaryTable userID={userID} />}
         </div>
