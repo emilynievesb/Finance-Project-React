f33b23ffd4dfcea242458072e8651277126142d9
emilynievesb <nemily1710@gmail.com>
2024-10-09 15:27:07 -0500

    feat: :sparkles: Formateo de fechas

--- src/components/TransactionsTable.jsx
+++ src/components/TransactionsTable.jsx
@@ -13,11 +13,7 @@ export default function TransactionsTable({ transactions, handleDeleteClick, ope
 
     // Función para formatear la fecha en DD/MM/YYYY
     const formatDate = (dateString) => {
-        const date = new Date(dateString);
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = date.getFullYear();
-        return `${day}/${month}/${year}`;
+        return dateString.replace(/-/g, '/');
     };
 
     return (
